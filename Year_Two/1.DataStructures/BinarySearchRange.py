4eb0fe8d6133584f9ed5f94d5de40dbea9f42786
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-03-26 02:31:33 -0700

    updated img for dataStructures

--- Year_Two/1.DataStructures/BinarySearchRange.py
+++ Year_Two/1.DataStructures/BinarySearchRange.py
@@ -42,8 +42,15 @@ def main():
 		if arg1 <= array[0] and arg2 >= array[len(array)-1]:
 			print("A",array)
 		else:
-			print("success")
-			print(array)
+			startRange = binarySearchStartRange(array, arg1)
+			endRange = binarySearchEndRange(array, arg2)
+			
+			if(startRange <= endRange):
+				#ensure meaningful result is printed
+				print("A[", startRange, "..", endRange, "]")
+			if(startRange > endRange):
+				print("null")
+			
 	else:
 		print("null")
 
@@ -59,7 +66,7 @@ def checkPreconditions(array, arg1, arg2):
 	else:
 		return True
 		
-def binarySearchAlgo(array, arg1, x):
+def binarySearchStartRange(array, x):
 	result = []
 	low = 0
 	high = len(array)-1
@@ -93,7 +100,39 @@ def binarySearchAlgo(array, arg1, x):
 				high = mid - 1
 	#could not find the element we were searching for (should never hit this b/c preconditions)
 	return -1
+	#end binarySearchStartRange
 
+def binarySearchEndRange(array, y):
+	low = 0
+	high = len(array)-1
+	while (low <= high):
+		mid = int(math.floor((low + high)/2))
+		if (array[mid] == y):
+			#hit the number we were looking for
+			if mid == len(array)-1:
+				#rightmost position in the array hit
+				return len(array)-1
+			elif array[mid+1] == y:	# ! what if arr[mid+1] is greater than arr.length-1?
+				#not the rightmost occurance of the number we were looking for
+				low = mid+1
+			else:
+				#returns the rightmost occurance of the number we were looking for
+				return mid
+		
+		elif array[mid] > y:
+			#we are too far to the right
+			if (mid - 1 <= y):
+				#we found the highest value within the range
+				return mid - 1
+			else:
+				#restrict the search area to left of mid
+				high = mid - 1
+		else:
+			#we are to the left of y, restrict the search area to the right of mid
+			low = mid + 1
+	#//could not find the element we were searching for (should never hit this b/c preconditions)
+	return -1
+#end binarySearchEndRange
     
 
 if __name__ == "__main__":
@@ -103,63 +142,7 @@ if __name__ == "__main__":
 public class BinarySearchRange {
 
 	public static void main(String[] args) {
-		/** Data processing input */
-		String fname = args[0]; //uses first argument entered as filename
-		//String fname = "./src/data.txt"; //alternatively, use this line from IDE to import data from file
-		File file = new File(fname);
-		Scanner scan = null;
-		try {
-			scan = new Scanner(file);
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
 		
-		if(!file.exists() && !file.canRead()) {
-			fname = args[0]; //uses first argument entered as filename
-			//fname = "./src/data.txt"; //alternatively, use this line from IDE to import data from file
-			file = new File(fname);
-		}
-		
-		int numOfLines = 0; //count the number of lines in the file
-		while(scan.hasNextLine()) {
-			scan.nextLine();
-			numOfLines++;
-		}
-		scan.close(); //clear buffer
-		
-		String[] strArr = new String[numOfLines]; //build a string array to hold file contents
-		
-		try {
-			Scanner fileRead = new Scanner(file);
-			for (int ctr = 0; ctr <= strArr.length-1; ctr++) { //store each line of file in string array element
-				strArr[ctr] = fileRead.nextLine();
-			}
-			fileRead.close();
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		int[] arr = new int[strArr.length]; //build int array of same size as string array
-		for (int ctr = 0; ctr <= strArr.length-1; ctr++) { //translate string array elements into int array elements
-			   arr[ctr] = Integer.parseInt(strArr[ctr]);
-		}
-		
-		int leftSearch = Integer.parseInt(args[1]); //take in second argument as string, parse as integer, pass to variable
-		int rightSearch = Integer.parseInt(args[2]); //take in third argument as string, parse as integer, pass to variable
-		
-		
-		if (arr.length < 1 || leftSearch > rightSearch) { // Check preconditions
-			//array is empty or left and right search inputs are reversed
-			System.out.println("null");
-		}
-		else if (leftSearch > arr[arr.length-1] || rightSearch < arr[0]) { // Check preconditions
-			//left search is greater than the largest number and right search is less than the smallest number in array
-			System.out.println("null");
-		}
-		else if (leftSearch <= arr[0] && rightSearch >= arr[arr.length-1]) { // Check preconditions
-			//the entire array falls within the search terms
-			System.out.println("A[" + 0 + ".." + (arr.length-1) + "]");
-		}
 		else { 
 			//terms are acceptable and a fragment of the array will be searched for
 			int startRange = binarySearchStartRange(arr, leftSearch);
@@ -256,15 +239,5 @@ public class BinarySearchRange {
 		//could not find the element we were searching for (should never hit this b/c preconditions)
 		return -1;
 	} //end binarySearchEndRange
-	/*
-	public static int countLines(Scanner infs) { //method not used in this version of program, future improvement plan
-		int count = 0;
-		while(infs.hasNextLine()) {
-			infs.nextLine();
-			count++;
-		}
-		return count;
-	} //end countLines method
-	*/
-} //end BinarySearchRange program
+
 """
