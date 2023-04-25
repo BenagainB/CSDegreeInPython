0f8b31b29eecb6fb370149a20f79ac78d7282121
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-24 22:33:57 -0700

    started unit tests

--- Year_Two/1.DataStructures/ass3_TestSinglyLinkedList.py
+++ Year_Two/1.DataStructures/ass3_TestSinglyLinkedList.py
@@ -10,13 +10,35 @@ class TestSinglyLinkedList(unittest.TestCase):
         self.node1 = Node(1, None, None)
         self.node2 = Node(2, None, None)
         self.node3 = Node(3, None, None)
-        print("Hello")
+        self.node4 = Node(4, None, None)
 
-    def test_get_next_success(self):
+    def test_creationOfInstances_success(self):
+        self.assertIsNotNone(self.my_linked_list)
+        self.assertIsNotNone(self.node1)
 
+    def test_nodeGetValue_success(self):
+        self.assertEqual(1, self.node1.get_value())            
+    
+    def test_nodeNextIsNone_success(self):
+        self.assertIsNone(self.node1.get_next())
+    
+    def test_linkedListEmpty_success(self):
+        self.assertIsNone(self.my_linked_list.head)
 
+    def test_addFirst_success(self):
+        self.my_linked_list.add_first(self.node1)
+        self.assertEqual("1 ", self.my_linked_list.to_string())
+        self.assertNotEqual("1", self.my_linked_list.to_string())
+        self.assertNotEqual("2", self.my_linked_list.to_string())
 
-
+    def test_linkedListSize_success(self):
+        self.assertEqual(0, self.my_linked_list.get_size())
+        self.my_linked_list.add_first(self.node1)
+        self.assertNotEqual(0, self.my_linked_list.get_size())
+        self.assertEqual(1, self.my_linked_list.get_size())
+        self.my_linked_list.add_first(self.node2)
+        self.assertNotEqual(1, self.my_linked_list.get_size())
+        self.assertEqual(2, self.my_linked_list.get_size())
 
 
 
