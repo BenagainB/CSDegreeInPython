c03b4a3ca7d13be24fc890186a0a5608e1e23fab
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-25 00:25:04 -0700

    fixed coverage report

--- Year_Two/1.DataStructures/test_SinglyLinkedList.py
+++ Year_Two/1.DataStructures/test_SinglyLinkedList.py
@@ -17,23 +17,23 @@ class TestSinglyLinkedList(unittest.TestCase):
         self.assertIsNotNone(self.node1)
 
     def test_nodeGetValue_success(self):
-        self.assertEqual(1, self.node1.get_value())            
-    
+        self.assertEqual(1, self.node1.get_value())
+
     def test_nodeSetValueCorrect_success(self):
         self.node1.set_value(2)
-        self.assertEqual(2, self.node1.get_value()) 
+        self.assertEqual(2, self.node1.get_value())
 
     def test_nodeSetValueNone_success(self):
         self.node1.set_value(None)
-        self.assertEqual(1, self.node1.get_value()) 
+        self.assertEqual(1, self.node1.get_value())
 
     def test_nodeNextIsNone_success(self):
         self.assertIsNone(self.node1.get_next())
-    
+
     def test_noneSetNext_success(self):
         self.node1.set_next(self.node2)
         self.assertEqual(self.node2, self.node1.get_next())
-    
+
     def test_linkedListEmpty_success(self):
         self.assertIsNone(self.my_linked_list.head)
 
@@ -56,24 +56,32 @@ class TestSinglyLinkedList(unittest.TestCase):
 # Use pytest to test all cases beginning with test_ (even if main is not defined)
 
 # pip install coverage
-# coverage run ass3_SinglyLinkedList.py
+# coverage run SinglyLinkedList.py
 # coverage report ass3_SinglyLinkedList.py
-    # Name                       Stmts   Miss  Cover
-    # ----------------------------------------------
-    # ass3_SinglyLinkedList.py     102     65    36%
-    # ----------------------------------------------
-    # TOTAL                        102     65    36%
-# coverage report -m ass3_SinglyLinkedList.py    
-    # Name                       Stmts   Miss  Cover   Missing
-    # --------------------------------------------------------
-    # ass3_SinglyLinkedList.py     102     65    36%   20-22, 37, 44-51, 55-60, 64-73, 77-81, 85-95, 99-106, 110-125, 134
-    # --------------------------------------------------------
-    # TOTAL                        102     65    36%
+    # Name                  Stmts   Miss  Cover
+    # -----------------------------------------
+    # SinglyLinkedList.py     103     84    18%
+    # -----------------------------------------
+    # TOTAL                   103     84    18%
+# coverage report -m SinglyLinkedList.py
+    # Name                  Stmts   Miss  Cover   Missing
+    # ---------------------------------------------------
+    # SinglyLinkedList.py     103     62    40%   43, 50-57, 61-66, 70-79, 83-87, 91-101, 105-112, 116-131, 140
+    # ---------------------------------------------------
+    # TOTAL                   103     62    40%
 # coverage html
     # generates htmlcov folder, view index.html to see clean report
 
 # pip install pytest-cov
-    # haven't figured out how to properly run this yet
+# pytest --cov=SinglyLinkedList
+    # test_SinglyLinkedList.py .........                                                    [100%]
+    # ---------- coverage: platform darwin, python 2.7.18-final-0 ----------
+    # Name                  Stmts   Miss  Cover
+    # -----------------------------------------
+    # SinglyLinkedList.py     103     62    40%
+    # -----------------------------------------
+    # TOTAL                   103     62    40%
+    # ========== 9 passed in 0.09 seconds ======================================================
 
 if __name__ == "__main__":
     unittest.main()
